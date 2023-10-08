970fc5833c60c986910cac87066435bc3f7c5dd8
iitz-i13 <98603364+iitz-i13@users.noreply.github.com>
2023-10-08 19:09:55 +0900

    :sparkles: タイムスタンプの追加, 残高確認画面への変数渡し

--- components/CategorizePage.js
+++ components/CategorizePage.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 
-const CategorizePage = ({ route }) => {
+const CategorizePage = () => {
   const navigation = useNavigation();
+  const route = useRoute();
 
   // InputPageから渡された数字を取得
-  const receivedAmount = route.params?.amount || '0';  // <-- ここで数字を取得
+  const receivedAmount = route.params?.amount || '0'; // <-- ここで数字を取得
+
+  const handleCategoryPress = category => {
+    const timestamp = new Date().toISOString();
+    navigation.navigate('残高確認画面', {
+      timestamp: timestamp,
+      category: category,
+      amount: receivedAmount,
+    });
+  };
 
   return (
     <View style={styles.container}>
@@ -14,31 +24,31 @@ const CategorizePage = ({ route }) => {
 
       <TouchableOpacity
         style={[styles.button, styles.expenseButton]}
-        onPress={() => navigation.navigate('残高確認画面')}>
+        onPress={() => handleCategoryPress('食費')}>
         <Text>食費</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
         style={[styles.button, styles.expenseButton]}
-        onPress={() => navigation.navigate('残高確認画面')}>
+        onPress={() => handleCategoryPress('交通費')}>
         <Text>交通費</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
         style={[styles.button, styles.expenseButton]}
-        onPress={() => navigation.navigate('残高確認画面')}>
+        onPress={() => handleCategoryPress('趣味')}>
         <Text>趣味</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
         style={[styles.button, styles.expenseButton]}
-        onPress={() => navigation.navigate('残高確認画面')}>
+        onPress={() => handleCategoryPress('その他')}>
         <Text>その他</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
         style={[styles.button, styles.incomeButton]}
-        onPress={() => navigation.navigate('残高確認画面')}>
+        onPress={() => handleCategoryPress('収入')}>
         <Text>収入</Text>
       </TouchableOpacity>
     </View>
