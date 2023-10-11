22930a0ccdf6e615abb6d859045822cc2e74a2f5
shoma <20rd053so@gmail.com>
2023-10-11 14:21:40 +0900

    update CategorizePage

---
+++ components/InputPage.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {useState} from 'react';
+
+const InputPage = () => {
+  const navigation = useNavigation();
+  const [input, setInput] = useState('');
+  const [previousInput, setPreviousInput] = useState('');
+  const [operation, setOperation] = useState(null);
+  const formatNumberWithCommas = number => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
+
+  const handlePress = value => {
+    let newInput = input + value;
+
+    if (input === '0' && value !== '0') {
+      setInput(formatNumberWithCommas(value));
+    } else if (input !== '0' || (input === '0' && value !== '0')) {
+      setInput(formatNumberWithCommas(newInput.replace(/,/g, '')));
+    }
+  };
+
+  const handleOperation = op => {
+    if (!input) return;
+    setPreviousInput(input);
+    setInput('');
+    setOperation(op);
+  };
+
+  const handleEqual = () => {
+    if (!previousInput || !input) return;
+
+    let result;
+    setInput(String(result));
+    setPreviousInput('');
+    setOperation(null);
+  };
+
+  const handleClear = () => {
+    setInput('');
+    setPreviousInput('');
+    setOperation(null);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.display}>
+        <Text style={styles.displayText}>{input}</Text>
+      </View>
+      <View style={styles.buttons}>
+        {['7', '8', '9', '4', '5', '6', '1', '2', '3', 'C', '0', '→'].map(
+          (button, index) => (
+            <TouchableOpacity
+              key={index}
+              style={styles.button}
+              onPress={() => {
+                if (button === 'C') {
+                  handleClear();
+                } else if (button === '→') {
+                  handleEqual();
+                } else {
+                  handlePress(button);
+                }
+              }}>
+              <Text style={styles.buttonText}>{button}</Text>
+            </TouchableOpacity>
+          ),
+        )}
+      </View>
+      <TouchableOpacity
+        style={styles.centerButton} // 修正したスタイル名
+        onPress={() => navigation.navigate('属性選択画面', {amount: '1000'})}>
+        <Text>Go to Categorize Page</Text>
+      </TouchableOpacity>
+
+      <View style={styles.footer}>
+        <TouchableOpacity
+          style={styles.footerButton} // 修正したスタイル名
+          onPress={() => navigation.navigate('残高確認画面')}>
+          <Text style={styles.buttonText}>Go to Result Page</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  centerButton: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: 'lightgray',
+    borderRadius: 5,
+  },
+
+  footerButton: {
+    // 新しいスタイル名
+    width: '100%',
+    padding: 20,
+    backgroundColor: 'lightgray',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontSize: 18,
+  },
+
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: 'lightgray',
+  },
+});
+
+export default InputPage;
---
+++ components/ResultPage.js
@@ -0,0 +1,112 @@
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, TouchableOpacity, FlatList} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+
+const ResultPage = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const [records, setRecords] = useState([]); // 記録を保存するためのstate
+
+  // CategorizePageから渡された情報を取得
+  const timestamp = route.params?.timestamp;
+  const category = route.params?.category;
+  const amount = route.params?.amount;
+
+  // タイムスタンプを月・日形式で表示するヘルパー関数
+  const formatTimestamp = timestamp => {
+    const date = new Date(timestamp);
+    return `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
+  };
+
+  React.useEffect(() => {
+    if (timestamp && category && amount) {
+      const newRecord = {
+        id: timestamp,
+        timestamp: formatTimestamp(timestamp),
+        category: category,
+        amount: amount,
+      };
+      setRecords(prevRecords => [newRecord, ...prevRecords]);
+    }
+  }, [timestamp, category, amount]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.balanceContainer}>
+        <Text style={styles.balanceText}>残高: ￥{amount}</Text>
+      </View>
+
+      {/* 記録のリストを表示 */}
+      <FlatList
+        data={records}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <View style={styles.recordRow}>
+            <Text>{item.timestamp}</Text>
+            <Text>{item.category}</Text>
+            <Text>{item.amount}</Text>
+          </View>
+        )}
+      />
+
+      <View style={styles.footer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('金額入力画面')}>
+          <Text style={styles.buttonText}>Go to Input Page</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+
+  balanceContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1.0,
+  },
+
+  balanceText: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+
+  button: {
+    width: '100%',
+    padding: 20,
+    backgroundColor: 'lightgray',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontSize: 18,
+  },
+
+  recordRow: {
+    flexDirection: 'row', // 横並びにする
+    justifyContent: 'space-between', // 各項目を均等に配置
+    padding: 10,
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray',
+  },
+
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: 'lightgray',
+  },
+});
+
+export default ResultPage;
