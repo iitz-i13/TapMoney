40b50bd8db73b2b672d47a132af515b8fd4f6223
iitz-i13 <98603364+iitz-i13@users.noreply.github.com>
2023-10-15 15:12:06 +0900

    :bug:

--- components/MemoPage.js
+++ components/MemoPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   View,
   TextInput,
@@ -7,48 +7,57 @@ import {
   KeyboardAvoidingView,
   ScrollView,
   Platform,
+  Text,
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const MemoPage = ({route, navigation}) => {
+const MemoPage = ({ route, navigation }) => {
   const [memo, setMemo] = useState(route.params?.item.memo || '');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const saveMemo = async () => {
-    const updatedRecord = {...route.params.item, memo: memo};
-
     try {
-      let storedRecords = JSON.parse(await AsyncStorage.getItem('records'));
+      let storedRecords = JSON.parse(await AsyncStorage.getItem('records')) || [];
+      const updatedRecord = { ...route.params.item, memo: memo };
       const index = storedRecords.findIndex(
         record => record.id === updatedRecord.id,
       );
-      storedRecords[index] = updatedRecord;
+
+      if (index !== -1) {
+        storedRecords[index] = updatedRecord;
+      } else {
+        storedRecords.push(updatedRecord);
+      }
+
       await AsyncStorage.setItem('records', JSON.stringify(storedRecords));
       navigation.navigate('残高確認');
-      } catch (error) {
-        console.error('Failed to save memo:', error);
+    } catch (error) {
+      console.error('Failed to save memo:', error);
+      setErrorMessage('メモの保存に失敗しました。');
     }
   };
 
   return (
     <KeyboardAvoidingView
-    style={styles.container}
-    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}>
-    <ScrollView>
-      <TextInput
-        autoFocus={true}
-        style={styles.textInput}
-        placeholder="メモを入力"
-        value={memo}
-        onChangeText={setMemo}
-        maxLength={100}
-        // multiline={true} // これを削除
-        onSubmitEditing={saveMemo} // これが"done"キーまたは"return"キーが押されたときに実行される
-        returnKeyType="done"
-      />
-      <Button title="保存" onPress={saveMemo} />
-    </ScrollView>
-  </KeyboardAvoidingView>
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}
+    >
+      <ScrollView>
+        <TextInput
+          autoFocus={true}
+          style={styles.textInput}
+          placeholder="メモを入力"
+          value={memo}
+          onChangeText={setMemo}
+          maxLength={100}
+          onSubmitEditing={saveMemo}
+          returnKeyType="done"
+        />
+        {errorMessage ? <Text style={{ color: 'red' }}>{errorMessage}</Text> : null}
+        <Button title="保存" onPress={saveMemo} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
