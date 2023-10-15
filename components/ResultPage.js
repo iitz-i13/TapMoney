852ad09f4bd28cf7637a3ea5b559b46184bf7cf0
iitz-i13 <98603364+iitz-i13@users.noreply.github.com>
2023-10-15 10:40:47 +0900

    :art: ヘッダーの色変更

--- components/ResultPage.js
+++ components/ResultPage.js
@@ -291,8 +291,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     padding: 10,
-    backgroundColor: 'white',
-    borderColor: 'white',
+    backgroundColor: '#EEEEEE',
+    // borderColor: 'lightgrey',
   },
 
   headerItem: {
